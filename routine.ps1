16df7a76e63f5874aefff253ee34f277679a8379
XxiAmAnthonyxX <140768078+XxiAmAnthonyxX@users.noreply.github.com>
2024-03-04 16:19:14 -0600

    Add files via upload

---
+++ routine.ps1
@@ -0,0 +1,120 @@
+MORNING 
+Wake Up am/pm
+
+Workout | 30 min
+    Monday 
+        -LOWER BODY
+            Abs
+            Glutes
+            Thighs
+            Hamstrings
+    Tuesday
+        -UPPER BODY
+            Arms
+            Back
+            Chest
+    Wednesday
+        -LOWER BODY
+            Abs
+            Glutes
+            Thighs
+            Hamstrings
+    Thurday
+        -UPPER BODY
+            Arms
+            Back
+            Chest
+    Friday
+        -LOWER BODY
+            Abs
+            Glutes
+            Thighs
+            Hamstrings
+    Saturday
+        -REST
+    Sunday
+        -REST
+
+
+
+Cold Shower | 15 min
+    -BATHING
+        Shampoo | 
+        Conditioner | 
+        Body Wash | 
+        Scrubber | 
+        Towel | 
+
+
+
+Morning Routine | 15 min
+    -ORAL
+        Toothpaste | 
+        Tooth Brush | 
+        Floss | 
+        Tongue Scraper | 
+        Mouthwash | 
+        Chapstick | 
+        Whitening Strips | 
+    -SKIN
+        Sunscreen | 
+        Moisterizer | 
+        Retinol | 
+        Cleanser | 
+    -MAINTENANCE
+        Nail Clipper | 
+        Razor |
+    -HANDS
+        Soap | 
+        Sanatizer | 
+        Lotion | 
+    -ODOR
+        Deodorant | 
+        Cologne | 
+
+
+
+ AFTERNOON 
+Meal 1 | 1h
+
+
+Leave 30 min
+
+
+Work | 5h
+
+
+Return 30 min
+
+
+Meal 2 | 1h
+
+ ACTIVITES 
+Vape Weed Or Eat Edibles
+Pleasure Myself Sexually
+Listen To Music Or Podcasts
+Research Topics Of Intrest
+Sharpen My Techknology Skills
+
+
+ EVENING 
+Training | 30 min
+
+
+Hot Shower | 15 min
+
+
+Evening Routine | 15 min
+
+
+ NIGHT 
+Read | 30 min
+
+
+Journal | 15 min
+
+
+Meditate | 15 min
+
+
+Go To Sleep am/pm
